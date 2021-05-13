07ec1829e388119a04c0ddba265db6d37336fe7b
jinwoo-jung <wlsdn4808@naver.com>
2021-05-13 16:29:59 +0900

    정진우 클릭이벤트생성

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -10,6 +12,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_jinwoo;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         String reset = "초기화";
 
         tv_jinwoo = findViewById(R.id.tv_jinwoo);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_jinwoo.setText("git");
+            }
+        });
 
 
 
