964712b2ac147797c3a624a4fe5ad201406220ad
songs4beat <kws951117@gmail.com>
2021-05-13 16:39:01 +0900

    testtset

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         int a = 1;
-        String reset = "초기화???";
+        String reset = "초기화?test??";
 
 
     }
